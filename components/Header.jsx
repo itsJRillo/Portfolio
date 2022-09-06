d503ed6797bca81a0c96be213951be3fad4b76e7
itsJRillo <jmorillolabour@gmail.com>
2022-09-06 20:37:11 +0200

    Cambio CV

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   const openResume = () => {
-    window.open("CV-Ismael Morillo.pdf");
+    window.open("CV-Ismael Morillo-Ingles.pdf");
   };
 
   return (
