07b506a1946483d7a28c724ea65a91221a6435d4
Ismael M <jmorillolabour@gmail.com>
2022-12-16 11:04:29 +0100

    Cypress Testing

---
+++ components/tests/ContactMe.cy.js
@@ -0,0 +1,12 @@
+import ContactMe from "../ContactMe.jsx";
+
+describe("<ContactMe>",() => {
+    beforeEach(() => {
+        cy.mount(<ContactMe/>)
+    })
+    
+    it("Mount the component", () => {
+        cy.mount(<ContactMe/>
+        )
+    })
+})
